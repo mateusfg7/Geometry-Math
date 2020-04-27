60822c8f2b10af561d9968a7719ad957aba77b0c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-27 17:10:12 -0300

    clean code

--- functions/triangle/TriangleArea.py
+++ functions/triangle/TriangleArea.py
@@ -7,7 +7,7 @@ h -> height
 '''
 
 
-def calculateTriangleAreaBH(b, h):
+def calculateTriangleAreaBH(b: float, h: float) -> float:
     a = (b*h)/2
     return a
 
@@ -19,14 +19,14 @@ c -> side C
 '''
 
 
-def calculateTriangleAreaSP(a, b, c):
-    semi_perimeter = (a+b+c)/2
+def calculateTriangleAreaSP(a: float, b: float, c: float) -> float:
+    semiPerimeter = (a+b+c)/2
 
-    sp_calc = semi_perimeter*(semi_perimeter-a) * \
-        (semi_perimeter-b)*(semi_perimeter-c)
+    spCalc = semiPerimeter*(semiPerimeter-a) * \
+        (semiPerimeter-b)*(semiPerimeter-c)
 
-    if sp_calc >= 0:
-        area = sqrt(sp_calc)
+    if spCalc >= 0:
+        area = spCalc ** 0.5
     else:
         area = 'numero negativo'
 
