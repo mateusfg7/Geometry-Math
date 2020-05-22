ed5e61ccdf47ff31e32daf76176dcd98aed09adc
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-22 15:46:31 -0300

    adding description in the 'calculateSquareArea' function

--- functions/square/SquareArea.py
+++ functions/square/SquareArea.py
@@ -1,10 +1,9 @@
-'''
-a -> area
-b -> base
-h -> height
-'''
-
-
 def calculateSquareArea(b: float, h: float) -> float:
+    '''
+    a -> area
+    b -> base
+    h -> height
+    '''
+
     a = b*h
     return a
