32a5503e9fa9c80245c19c2559514b3679944828
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-22 15:48:33 -0300

    updating description in the 'calculateSquareArea' function

--- functions/square/SquareArea.py
+++ functions/square/SquareArea.py
@@ -1,8 +1,8 @@
 def calculateSquareArea(b: float, h: float) -> float:
     '''
-    a -> area
-    b -> base
-    h -> height
+    param 1: b -> base\n
+    param 2: h -> height\n
+    return: a -> area\n
     '''
 
     a = b*h
