26e6bfacd633f713ff2fe87f3cdb32041ad0ff7f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-25 09:57:37 -0300

    adding types and description in the 'circleAreaMenu' function #2

--- functions/circle/__init__.py
+++ functions/circle/__init__.py
@@ -1,10 +1,15 @@
+from typing import NoReturn
+
 from math import pi
 
 from components.header import header
 from functions.circle.CircleArea import circleArea
 
 
-def circleAreaMenu():
+def circleAreaMenu() -> NoReturn:
+    '''
+    Shows header and options to calculate the area of the circle.
+    '''
     header()
 
     print('A =π.r²\n')
